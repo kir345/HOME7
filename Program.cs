10f652200442b2582cafe3616463ca4d2f65bc71
kir345 <123649165+kir345@users.noreply.github.com>
2023-03-07 18:22:26 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,44 +1,33 @@
-﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает 
-//значение этого элемента или же указание, что такого элемента нет.
+﻿//  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 //Например, задан массив:
 //1 4 7 2
 //5 9 2 3
 //8 4 2 4
-//17 -> такого числа в массиве нет
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int rows = ReadInt("Введите номер строки: ");
-int colums = ReadInt("Введите номер столбца: ");
-int[,] nums = new int[3,4];
-FillArray(nums);
-PrintArray(nums);
+using System;
+using static System.Console;
 
-if (rows < nums.GetLength(0) && colums < nums.GetLength(1)) Console.WriteLine(nums[rows, colums]);
-else Console.WriteLine($"{rows}{colums} - такого числа в массиве нет");
+int[,] arr = new int[3,4];
 
-void FillArray(int[,] array)
+for (int i = 0; i < arr.GetLength(0); i++)
 {
-    for(int k = 0; k < array.GetLength(0); k++)
+    for (int j = 0; j < arr.GetLength(1); j++)
     {
-        for(int i = 0; i < array.GetLength(1); i++)
-        {
-            array[k,i] = new Random().Next(1,10);
-        }
+        arr[i, j] = new Random().Next(1, 10);
+        Write(arr[i, j] + " ");
     }
+    WriteLine();
 }
-void PrintArray(int[,] array)
+WriteLine(arr.GetLength(0));
+
+for (int j = 0; j < arr.GetLength(1); j++)
 {
-    for(int k = 0; k < array.GetLength(0); k++)
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for(int i = 0; i < array.GetLength(1); i++)
-        {
-            Console.Write(array[k,i] + " ");
-        }
-        Console.WriteLine();
+        sum += arr[i, j];
     }
-    Console.WriteLine();
+    Write($"{sum / arr.GetLength(0)} ");
 }
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+ReadLine();
