8306b16bf983cc368e3005b1478ff39ad5053b05
kir345 <123649165+kir345@users.noreply.github.com>
2023-03-14 19:05:48 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,39 +1,33 @@
-﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-//m = 3, n = 4.
-//0,5 7 -2 -0,2
-//1 -3,3 8 -9,9
-//8 7,8 -7,1 9
+﻿//  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int rows = ReadInt("Введите количество строк: ");
-int colums = ReadInt("Введите количество столбцов: ");
-double[,] nums = new double[3,4];
-FillArray(nums);
-PrintArray(nums);
+using System;
+using static System.Console;
 
-void FillArray(double[,] array)
+int[,] arr = new int[3,4];
+
+for (int i = 0; i < arr.GetLength(0); i++)
 {
-    for(int k = 0; k < array.GetLength(0); k++)
+    for (int j = 0; j < arr.GetLength(1); j++)
     {
-        for(int i = 0; i < array.GetLength(1); i++)
-        {
-            array[k,i] = new Random().Next(10);
-        }
+        arr[i, j] = new Random().Next(1, 10);
+        Write(arr[i, j] + " ");
     }
+    WriteLine();
 }
-void PrintArray(double[,] array)
+WriteLine(arr.GetLength(0));
+
+for (int j = 0; j < arr.GetLength(1); j++)
 {
-    for(int k = 0; k < array.GetLength(0); k++)
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for(int i = 0; i < array.GetLength(1); i++)
-        {
-            Console.Write(array[k,i] + " ");
-        }
-        Console.WriteLine();
+        sum += arr[i, j];
     }
-    Console.WriteLine();
+    Write($"{sum / arr.GetLength(0)} ");
 }
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+ReadLine();
